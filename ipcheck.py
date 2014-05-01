2a78498c1574d7301eb62a1395c8d77e8376dcd9
GitMiku <MikuMiku@lavabit.com>
2014-05-01 19:38:44 +0200

    Update ipcheck.py

--- ipcheck.py
+++ ipcheck.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+""" Python tool that gets your external IP. Checks different sites until one works. """
+
 import urllib2
 import sys
 def getIP(URL):
@@ -12,12 +14,25 @@ def getIP(URL):
                 return
 
 if __name__ == '__main__':
+        getIP("http://whatismyip.akamai.com/")
         getIP("http://curlmyip.com")
         getIP("http://ip.telize.com/")
         getIP("http://tnx.nl/ip")
         getIP("http://myip.dnsomatic.com/")
         getIP("http://ident.me/")
         getIP("http://ifconifg.me/ip")
-        getIP("http://whatismyip.akamai.com/")
         getIP("http://icanhazip.com/")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
